2a4ca40a21e88f4c09703757393bdb8f8a7261a2
SheyMouse <sheymouse@gmail.com>
2012-06-27 22:26:47 +0100

    v0.2 of the adventue game
    Assets used when designing the game too.

---
+++ README.txt
@@ -0,0 +1,38 @@
+##           A Simple Text Adventure                                         ##
+##                  Version 0.1                                              ##
+##                                                                           ##
+##      Author: SheyMouse - June 2012                                        ##
+##                                                                           ## 
+##  Exercise 36 from Learn Python the Hard Way by Z. A.Shaw                  ##
+##              http://learnpythonthehardway.org                             ##
+##                                                                           ##
+##  A simple text adventure which takes a player linearly through a set of   ##
+##  rooms.                                                                   ##
+##  Each room has an obstacle to get past, with one solution, one failure to ##
+##  take you back to the beginning, and a second fail to kill you.           ##
+##                                                                           ##
+##  This looks like a "My first game" adventure. That's because it is. Do    ##
+##  not expect Zork on the first attempt.                                    ##
+##                                                                           ##
+###############################################################################
+
+v0.2
+Progress and bug fixes
+
+Some logic bugs fixed
+Player can now go all the way through the game.
+
+Also as part of this checkin, I have added a resources folder which contains the flow diagram
+and mind map I used to help design the game.
+
+
+
+v0.1
+Initial version
+
+The game can be played end-to-end, but there are a couple of bugs which still 
+need to be sorted out.
+Also, there is still a lot of debug story text which needs to be removed and the
+story entered. 
+There is a TODO section of the code. See that for my wish list, and bugs when I can
+be bothered to update it.
--- SimpleTextAdventure.py
+++ SimpleTextAdventure.py
@@ -1,5 +1,5 @@
 ##           A Simple Text Adventure
-##                  Version 0.1
+##                  Version 0.2
 ##
 ##      Author: SheyMouse - June 2012
 ##
@@ -16,17 +16,26 @@
 ##
 ################################################################################
 
-## TODO LIST - Global list which I would like to do in the future
+## TODO LIST - Global list of tasks (when I remember to update them!)
 ##
-## X Make a single runction for the raw input prompt and call that each time
-## - Fix the calling of the prompt function. It kind of works, but breaks the
+##  - Add a list of possible solutions or phrases that will lead to traps
+##  - Fix the calling of the prompt function. It kind of works, but breaks the
 ##   game somehow. Find out why
-## - Write a function to look for "open door" in phrase. Then call that.
-## X Find out how to call the exit_game function and have the game exit properly
-## - Make the open door phrase smarter so it can handle any formatted string
+##  - Write a function to look for "open door" in phrase. Then call that.
+##  - Make the open door phrase smarter so it can handle any formatted string
 ##   containing the letters open door
-## - Insead of dog_moved, dwarf_moved, etc. could I make a general it_moved
+##  - Insead of dog_moved, dwarf_moved, etc. could I make a general it_moved
 ##   function which is set to false at the beginning of a room?
+##  - Place all text in another file and call what is needed. I think this will
+##    make it cleaner.
+##  - Indent text by four characters to make it easier to read on screen
+##  - Trailing new line on all blocks of text
+##  - Add pauses to some of the text for a pause effect. Mainly because I can.
+##  - Having "if axe in" keyword check in two rooms produces a bug since the
+##    code reads top-down. Perhaps add an axe_got boolean in dwarf room to 
+##    make the program read the second "axe" in the dragon room
+##  X Make a single runction for the raw input prompt and call that each time
+##  X Find out how to call the exit_game function and have the game exit properly
 ##
 ################################################################################
 
@@ -40,22 +49,30 @@ from sys import exit
 
 # Room 1 - Intro room
 def start():
-    print "You are in a dimly lit room."
-    print "There is a door in front of you."
-    print "An inscription reads \"Welcome to my incredibly complicated maze\""
-    print "Type \"Open door\" if you dare!\n"
+    print "\n\tYou wake in a dimly lit room."
+    print "\tThere is a door in front of you."
+    print "\tAn inscription reads \"Welcome to my incredibly complicated maze\""
+    print "\tType \"Open door\" if you dare!\n"
     
     next = raw_input("> ")
+
+#BUG - Typing gibberish no longer leads to death? Why?
     
-    if next == "open door" or "Open door":
+    if next == "open" or "Open":
         dog_room()
+    elif "exit" in next:            
+        exit_game()
     else:
         dead("You cannot follow a simple instruction!")
 
 # Room 2 - Dog gnawing on bone room
 def dog_room():
-#TODO - Add some intro text
-    print "You are in the dog room"
+
+    print "\n\tYou enter the next room. The door locks behind you."
+    print "\tYou see a large dog in front of another door."
+    print "\tHe is gnawing on a bone."
+    print "\tThe only way through that door is to move him first."
+    print "\tBut how?\n"
 
     dog_moved = False
 
@@ -68,59 +85,76 @@ def dog_room():
         elif next == "taunt dog":
             dead("Your excessive waving annoyed the dog and he attacked you.")
         elif next == "take bone":
-            #tease dog
-            # throw bone
-            print "DEBUG TEXT - Dog moved"
+            print "\n\tIn a moment of madness, you snatch the bone from the dog."
+            print "\tBefore he rips you to shreds, you throw the bone and shout"
+            print "\t\"Fetch!\""
+            print "\tThe dog bounds eagerly after the bone.\n"
+            
             dog_moved = True
         elif next == ("open door" or "Open door") and dog_moved:
             dwarf_room()
-        elif next != ("open door" or "Open door") and dog_moved:
-            dead("Why did you have to go and say that")
         elif next == "exit":            
             exit_game()
         else:
-            dead("You typed something I don't understand")
+            dead("\t...is the wrong command.\n")
         
 # Room 3 - Dwarf room
 def dwarf_room():
 #TODO - Add some intro text about being stared up
-    print "You are in the dwarf room"
-
-    dragon_moved = False
+    print "\n\tYou look around the new room as the sounds of the slamming door"
+    print "\techos behind you."
+    print "\tYou see a dwarf guarding the next door."
+    print "\tHe, she, er, it, is looking at you intently."
+    print "\tYou get the feeling the dwarf is trying to stare you up. (dwarves"
+    print "\tare not able to stare down many people; even most animals are"
+    print "\tout of the question)\n"
+    print "\tYou need to wipe that stare of it's face.\n"
+    
+    
+    dwarf_moved = False
     
     while True:
         next = raw_input("> ")
 
         if "talk" in next:
-            print "You try to reason with the dwarf."
-            print "Unfortunately, your drarfish is very rusty and you insult her."
-            print "She walks over and smacks you in the head with the side of her axe."
-            print "As you lose conciousness you think to yourself how lucky you are that"
-            print "that she used the flat side and not the sharp side"
+            print "\n\tYou try to reason with the dwarf."
+            print "\tUnfortunately, your drarfish is very rusty and you"
+            print "\tinsult it."
+            print "\tThe dwarf walks over and smacks you in the head with the
+            print "\nside of the axe."
+            print "\tAs you lose conciousness you think to yourself how lucky"
+            print "\tyou are that the flat side and not the sharp side connected"
+            print "\twith your noggin.\n"
             start()
         elif "push" in next:
-            dead("Yeah, not a good idea pushing someone half your size around.")
-        elif "axe" in next:
-            # take axe
-            # dwarf upset and runs away
-            print "DEBUG TEXT - Dwarf moved"
-            dragon_moved = True
-        elif next == ("open door" or "Open door") and dragon_moved:
+            dead("Despite being half your size, pushing dwarfs around is never a good idea.")
+        elif "take axe" in next:
+            print "\n\tEmboldened by your perfomance in the last room, you"
+            print "\tsnatch the axe from the grasp of the dwarf."
+            print "\tThis upsets the dwarf."
+            print "\tA lot."
+            print "\tYou feel like a bully, but only for the moment."
+            print "\tBecause, the door is clear. And you have an axe."
+            print "\t..." # insert delay here
+            print "\tNo, you are not going to do \"a Shining\" on the door." 
+            
+            dwarf_moved = True
+        elif next == ("open door" or "Open door") and dwarf_moved:
             dragon_room()
-        elif next != ("open door" or "Open door") and dragon_moved:
-            dead("Why did you have to go and say that")            
+        #elif next != ("open door" or "Open door") and dwarf_moved:
+         #   dead("Why did you have to go and say that?")            
         elif next == "exit":            
             exit_game()
         else:
-            dead("You typed something I don't understand")            
+            dead("You typed something I don't understand\n")            
             
 # Room 4 - Dragon room
 def dragon_room():
 # TODO - Intro text. Dragon asleep. On gold. How will you move it.
-    print "You are in the dragon room"
+    print "You are in the dragon room\n"
 
     dragon_moved = False
-    while False:
+    while True:
         next = raw_input("> ")
 
         if next == "noise":
@@ -129,8 +163,9 @@ def dragon_room():
             # you will have visions of those claws coming at you for ever
             start()
         elif "slap" in next:
-            dead("The dragon casually send out a flame whch toasts you alive!")
-        elif "axe" in next:
+            print "You slap the dragon"
+            dead("In return, the dragon casually sends out a flame whch toasts you alive!")
+        elif next == "chop":
             #dragon sees axe
             #dragon gets scared
             #dragon runs away
@@ -139,21 +174,25 @@ def dragon_room():
         elif next == "open door":
             freedom()
         elif next == "gold open door":
-            freedom()
+            freedom_gold()
         elif next == "exit":            
             exit_game()
         else:
-            dead("You typed something I don't understand")
+            dead("\tYou typed something I don't understand\n")
             
 # Room 5 - Exit room
 def freedom():
     print "You are free!"
 
     exit_game()
+def freedom_gold():
+    print "You are free. And you have gold."
+
+    exit_game()
     
 # Dead - Tell person they are dead and quit game
 def dead(cause):
-    print cause, "\nNow you are dead"
+    print cause, "Now you are dead\n"
     exit(0)
     
 # Exit - Quit game
