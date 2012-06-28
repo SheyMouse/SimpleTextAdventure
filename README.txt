7b04aac412ad1c87388d20fad817c45f1a435d64
SheyMouse <git@github.com:SheyMouse/SimpleTextAdventure.git>
2012-06-28 13:24:06 +0100

    v0.3 Full game logic is in. Can play all the way through However, there are still some bugs (commented in code) There is also a long TODO list. Working on cheat keyword next.

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@
 ##                                                                           ##
 ##      Author: SheyMouse - June 2012                                        ##
 ##                                                                           ## 
-##  Exercise 36 from Learn Python the Hard Way by Z. A.Shaw                  ##
+##  Written as part ofExercise 36 from Learn Python the Hard Way by Z. A.Shaw##
 ##              http://learnpythonthehardway.org                             ##
 ##                                                                           ##
 ##  A simple text adventure which takes a player linearly through a set of   ##
@@ -16,6 +16,11 @@
 ##                                                                           ##
 ###############################################################################
 
+v0.3
+Full game logic is in, complete with cheesy text.
+Still a lot ot do, as can be seen by the TODO list at the top, but the basic
+game works. 
+
 v0.2
 Progress and bug fixes
 
@@ -24,8 +29,6 @@ Player can now go all the way through the game.
 
 Also as part of this checkin, I have added a resources folder which contains the flow diagram
 and mind map I used to help design the game.
-
-
 
 v0.1
 Initial version
--- SimpleTextAdventure.py
+++ SimpleTextAdventure.py
@@ -1,9 +1,9 @@
 ##           A Simple Text Adventure
-##                  Version 0.2
+##                  Version 0.3
 ##
 ##      Author: SheyMouse - June 2012
 ##
-##  Exercise 36 from Learn Python the Hard Way by Z. A.Shaw
+##  Written as part of Exercise 36 from Learn Python the Hard Way by Z. A.Shaw
 ##              http://learnpythonthehardway.org
 ##
 ##  A simple text adventure which takes a player linearly through a set of
@@ -19,6 +19,8 @@
 ## TODO LIST - Global list of tasks (when I remember to update them!)
 ##
 ##  - Add a list of possible solutions or phrases that will lead to traps
+##      - Split up the verb and object into separate lists to make the solutions
+##          more robust
 ##  - Fix the calling of the prompt function. It kind of works, but breaks the
 ##   game somehow. Find out why
 ##  - Write a function to look for "open door" in phrase. Then call that.
@@ -34,6 +36,7 @@
 ##  - Having "if axe in" keyword check in two rooms produces a bug since the
 ##    code reads top-down. Perhaps add an axe_got boolean in dwarf room to 
 ##    make the program read the second "axe" in the dragon room
+##  
 ##  X Make a single runction for the raw input prompt and call that each time
 ##  X Find out how to call the exit_game function and have the game exit properly
 ##
@@ -120,8 +123,8 @@ def dwarf_room():
             print "\n\tYou try to reason with the dwarf."
             print "\tUnfortunately, your drarfish is very rusty and you"
             print "\tinsult it."
-            print "\tThe dwarf walks over and smacks you in the head with the
-            print "\nside of the axe."
+            print "\tThe dwarf walks over and smacks you in the head with the"
+            print "\tside of the axe."
             print "\tAs you lose conciousness you think to yourself how lucky"
             print "\tyou are that the flat side and not the sharp side connected"
             print "\twith your noggin.\n"
@@ -134,9 +137,9 @@ def dwarf_room():
             print "\tThis upsets the dwarf."
             print "\tA lot."
             print "\tYou feel like a bully, but only for the moment."
-            print "\tBecause, the door is clear. And you have an axe."
-            print "\t..." # insert delay here
-            print "\tNo, you are not going to do \"a Shining\" on the door." 
+            print "\tBecause, the door is clear. And you have an axe.\n" # insert delay here
+            print "\t...\n" # insert delay here
+            print "\tNo, you are not going to do a \"Shining\" on the door.\n" 
             
             dwarf_moved = True
         elif next == ("open door" or "Open door") and dwarf_moved:
@@ -151,29 +154,39 @@ def dwarf_room():
 # Room 4 - Dragon room
 def dragon_room():
 # TODO - Intro text. Dragon asleep. On gold. How will you move it.
-    print "You are in the dragon room\n"
+    print "\n\tYou enter what feels like the final room."
+    print "\tBlocking the door this time is a great big dragon."
+    print "\tIn keeping with mythical creature stereotype, the dragon is sitting on a"
+    print "\tgreat big pile of gold."
+    print "\tHow are you going to part a dragon with its gold, and therefore free the"
+    print "\tdoor up for you to go through?\n"
 
     dragon_moved = False
     while True:
         next = raw_input("> ")
 
         if next == "noise":
-            # dragon annoyed
-            # dragon slaps you into unconciousness
-            # you will have visions of those claws coming at you for ever
+            print "\n\tYou make a noise that you hope will scare the dragon away."
+            print "\tThis just annoys the dragon."
+            print "\tIt slaps you into unconciousness."
+            print "\tThe sight of those claws coming towards you will haunt you "
+            print "\tforever.\n"
+            
             start()
         elif "slap" in next:
             print "You slap the dragon"
-            dead("In return, the dragon casually sends out a flame whch toasts you alive!")
-        elif next == "chop":
-            #dragon sees axe
-            #dragon gets scared
-            #dragon runs away
-            print "DEBUG - Dragon has moved"
+            dead("In return, the dragon casually sends out a flame whch toasts \nyou alive!\n")
+        elif next == "use axe":
+            print "\n\tYou remember you are still carrying an axe."
+            print "\tYou heft the axe and begin to walk towards the dragon."
+            print "\tSensing something is wrong, the dragon opens its eyes."
+            print "\tUpon seeing your big axe, the dragon lets out a scream"
+            print "\tand bolts, leaving its gold behind and the door free to walk through.\n"
+            
             dragon_moved
         elif next == "open door":
             freedom()
-        elif next == "gold open door":
+        elif next == "take gold":
             freedom_gold()
         elif next == "exit":            
             exit_game()
@@ -182,28 +195,45 @@ def dragon_room():
             
 # Room 5 - Exit room
 def freedom():
-    print "You are free!"
+    print "\n\tYou open the door and see your way out."
+    print "\tAs you walk through the door it slams behind you."
+    print "\tAnd then you remember the pile of gold.\n" # add a pause here
+    print "\tUnguarded.\n" # add a pause here
 
+    print "\tBut at least you have an axe.\n"
+
+    print "\n\n\t\tTHE END\n"
+    
     exit_game()
+    
 def freedom_gold():
-    print "You are free. And you have gold."
+    print "\n\tWith the dragon gone, the pile of shining gold seems to call to you"
+    print "\tYou go over to the pile of gold, and fill your pockets, and take as much"
+    print "\tof the lovely stuff in your arms."
+    print "\tYou stagger over to the door and attempt to open the door."
+    print "\tAfter several attempts, you manage to open the door, while still carrying"
+    print "\ta load of gold.\n"
+    print "\tYou are free.\n"
+    print "\tAnd rich.\n"
 
+    print "\tNow, if only you knew where you were...\n"
+
+    print "\n\n\t\tTHE END\n"
     exit_game()
     
 # Dead - Tell person they are dead and quit game
 def dead(cause):
-    print cause, "Now you are dead\n"
+    print cause, "\n\tNow you are dead\n"
     exit(0)
     
 # Exit - Quit game
 def exit_game():
-    print "Thanks for playing!\n"
+    print "\n\tThanks for playing!\n"
     exit(0)
 
 #Start - Calls the start function
 start()
 
-# Hint - Brings up some ideas to help the user out
 # Cheat - Brings up solution to room
 
 
